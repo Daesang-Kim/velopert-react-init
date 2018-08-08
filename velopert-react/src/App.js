5e5fb6a1c02680db5705661808cdd1196048ab71
Thor.Kim <kimdaesang@gmail.com>
2018-08-09 00:55:17 +0900

    [공통][React] 프로젝트 생성

---
+++ velopert-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+   
+    return (
+      <div className='App'>
+        안녕하세요 Hi there!
+      </div>
+    );
+  }
+}
+
+export default App;
