f75fe05e05576a5bcf6f3d10498b921e517ab424
Thor.Kim <kimdaesang@gmail.com>
2018-08-27 00:53:45 +0900

    [react][velopert] 기초 6장부터 7장까지. 진행

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PhoneForm from './components/PhoneForm'
+import PhoneInfoList from './components/PhoneInfoList';
 import './App.css';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
   render() {
+    const { information } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
