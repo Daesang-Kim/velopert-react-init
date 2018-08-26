888002956ce8e3a22ff618f6e41b1cc14f13cfa5
Thor.Kim <kimdaesang@gmail.com>
2018-08-27 00:24:02 +0900

    [react][velopert] 6장부터 시작합니다

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -9,12 +9,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <div>{
-          1 + 1 === 2 
-            ? (<div>맞아요!</div>)
-            : (<div>틀려요!</div>)
-          }
-          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
