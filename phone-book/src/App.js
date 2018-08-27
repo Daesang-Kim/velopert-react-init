3c3ff2ee410c73502d6b9b67b1e8c9985aa7d6a1
daesang.kim <daesang.kim@hanwha.com>
2018-08-27 09:37:18 +0900

    [react][velopert] 누구든지 하는 리액트 8편 중 데이터 삭제 기능 완료

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -25,6 +25,14 @@ class App extends Component {
       information: information.concat({ id: this.id++, ...data })
     })
   }
+  
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
   render() {
     const { information } = this.state;
     return (
@@ -32,7 +40,7 @@ class App extends Component {
         <PhoneForm
           onCreate={this.handleCreate}
         />
-        <PhoneInfoList data={this.state.information}/>
+        <PhoneInfoList data={this.state.information} onRemove={this.handleRemove}/>
       </div>
     );
   }
